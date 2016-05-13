702370c0a7a1a4546b48cb62ab75e301091f134a
Preechai Mekbungwan <preechaim@gmail.com>
2016-05-13 20:39:10 +0700

    Initial commit

---
+++ app/src/main/java/asia/ait/interlab/sen/testchat.java
@@ -0,0 +1,13 @@
+package asia.ait.interlab.sen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class testchat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testchat);
+    }
+}
